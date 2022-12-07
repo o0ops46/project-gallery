1d98124630078b88ca530a93ae7c9a6e7cc1cdda
o0ops <mariantdm@icloud.com>
2022-12-07 21:53:24 +0000

    flexbox and added event listener to every button

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+const detailsBtn = document.querySelectorAll('.details-button');
+
+const getDescriptions = function () {
+  document.create;
+};
+
+detailsBtn.forEach(element => {
+  element.addEventListener('click', function (e) {
+    console.log(e);
+  });
+});
