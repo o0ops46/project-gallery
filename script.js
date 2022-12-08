b57b724c123e61fca9abc608dd358781be01bf27
o0ops <mariantdm@icloud.com>
2022-12-08 20:58:45 +0000

    added a subtitle

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 'use strict';
-const detailsBtn = document.querySelectorAll('.details-button');
-
-const getDescriptions = function () {
-  document.create;
-};
-
-detailsBtn.forEach(element => {
-  element.addEventListener('click', function (e) {
-    console.log(e);
-  });
-});
